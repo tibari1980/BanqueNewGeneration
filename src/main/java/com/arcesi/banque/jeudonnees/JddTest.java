ce063868b0774afdfcc4d50404dfdc22389079c3
tibarinewdzign@gmail.com <tibarinewdzign@gmail.com>
2023-03-04 13:07:52 +0100

    Ajout d'un établissement

--- src/main/java/com/arcesi/banque/jeudonnees/JddTest.java
+++ src/main/java/com/arcesi/banque/jeudonnees/JddTest.java
@@ -90,6 +90,14 @@ public class JddTest implements CommandLineRunner {
 				.libelleEtablissement("Banque Tibari Nouvelle Génération").build();
 		EtablissementBean etablissementSaved = etablissementRepository.save(newEtablissement);
         
+		EtablissementBean newEtablissementTest = EtablissementBean.builder().codeEtablissement("10099")
+				.createdAt(Instant.now()).updatedAt(null)
+				.codeEtablissementUnique(UUID.randomUUID().toString().replace("-", ""))
+				.domiciliation("07 RUE PEGUY 75006 PARIS ").emailEtablissement("newbanqueenligne@gmail.com")
+				.telephoneEtablissement("0625491640").faxEtablissement("0121201214").codeGuiche("00040")
+				.libelleEtablissement("Banque Tibari Nouvelle Génération").build();
+		
+		EtablissementBean etablissementSavednew = etablissementRepository.save(newEtablissement1);
 		//insertion des groupes
 		//ajouter des groupes
 		ajouterLesGroupes();
